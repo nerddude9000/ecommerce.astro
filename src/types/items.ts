80493f04dc9e165b924fb4325c3cc22880c8a794
nerddude <30808467+nerddude24@users.noreply.github.com>
2025-11-11 21:55:12 +0100

    add fake api

--- src/types/items.ts
+++ src/types/items.ts
@@ -1,11 +1,19 @@
+type Category =
+	| "parts"
+	| "accessories"
+	| "cases"
+	| "peripherals"
+	| "desks"
+	| "chairs";
+
 type Item = {
 	id: number;
 	name: string;
 	desc: string;
 	price: number;
 	stock: number;
-};
-
-type GraphicalItem = Item & {
+	category: Category;
 	imgs: string | string[];
 };
+
+export type { Item, Category };
