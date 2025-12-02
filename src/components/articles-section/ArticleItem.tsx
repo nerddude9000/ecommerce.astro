543bdb5053dcf48551e16d4065e8e868083a5b9c
nerddude <30808467+nerddude24@users.noreply.github.com>
2025-12-02 21:09:08 +0100

    fix: img console errors

--- src/components/articles-section/ArticleItem.tsx
+++ src/components/articles-section/ArticleItem.tsx
@@ -8,7 +8,7 @@ export default function ArticleItem({ article }: Props) {
 	return (
 		<article className="max-w-64 flex flex-col gap-2 bg-neutral-800 p-2 rounded-2xl">
 			<div className="w-full h-64">
-				<img src={article.img} alt="" />
+				<img src={article.img !== "" ? article.img : undefined} alt="" />
 			</div>
 
 			<h1 className="text-lg font-black italic">{article.name}</h1>
