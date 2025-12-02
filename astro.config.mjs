3e97c1bdd71e214f0a196174da3fbf2dab5607fc
nerddude <30808467+nerddude24@users.noreply.github.com>
2025-12-02 22:21:16 +0100

    feat: add poppins font

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
@@ -10,5 +10,13 @@ export default defineConfig({
 
   vite: {
     plugins: [tailwindcss()]
+  },
+
+  experimental: {
+    fonts: [{
+      provider: fontProviders.google(),
+      name: "Poppins",
+      cssVariable: "--font-poppins"
+    }]
   }
 });
